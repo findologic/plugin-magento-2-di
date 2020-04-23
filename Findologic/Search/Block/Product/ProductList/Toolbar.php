acb7152d5c5b4186a5021ef83bdb09c7eee15389
tobiasgraml11 <t.graml@findologic.com>
2020-04-23 14:58:15 +0200

    Fix some warnings

--- Findologic/Search/Block/Product/ProductList/Toolbar.php
+++ Findologic/Search/Block/Product/ProductList/Toolbar.php
@@ -4,12 +4,6 @@ namespace Findologic\Search\Block\Product\ProductList;
 
 use Magento\Framework\View\Element\Template;
 
-/**
- * Class Toolbar
- *
- * @package Findologic\Search\Block\Product\ProductList
- * @author Ing Software
- */
 class Toolbar extends Template
 {
 
